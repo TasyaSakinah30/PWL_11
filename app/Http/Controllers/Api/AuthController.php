c0a0876153c77082b792f695ff9ef2ee7379299a
tasya <tasya00661@gmail.com>
2021-04-20 18:04:39 +0700

    AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Traits\ApiResponse;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
